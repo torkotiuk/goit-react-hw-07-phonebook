4eb7b7ff993ba0a3ccd395aa33e1404d714a3ae0
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-05-21 20:26:09 +0200

    clear structure

--- zz/_add_finished/redux _add_finished/phonebook/contacts-actions.js
+++
@@ -1,8 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const addContactRequest = createAction('contacts/addContactRequest');
-export const addContactSuccess = createAction('contacts/addContactSuccess');
-export const addContactFailure = createAction('contacts/addContactError');
-
-export const changeFilter = createAction('contacts/changeFilter');
-export const deleteContact = createAction('contacts/Delete');
--- zz/beforeFetch/components/Stat/Stat-logic.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import Stat from './Stat';
-
-const mapStateToProps = state => ({
-  total: state.contacts.items.length,
-});
-
-export default connect(mapStateToProps)(Stat);
